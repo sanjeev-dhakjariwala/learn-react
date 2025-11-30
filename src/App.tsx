9084d5113acee20f09161988f49a11721c12782b
sanjeev-dhakjariwala <sanjeevjha882@gmail.com>
2025-11-30 20:13:07 +0530

    Set /home as default route and add product-details page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Header } from "./container/header/Header";
 import { HomePage } from "./container/home/HomePage";
 import { ElectronicsPage } from "./container/electronics/ElectronicsPage";
@@ -11,7 +11,9 @@ function App() {
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<Navigate to="/home" replace />} />
+        <Route path="/home" element={<HomePage />}/>
+        <Route path="/home/:id" element/>
         <Route path="/electronics" element={<ElectronicsPage />} />
         <Route path="/groceries" element={<GroceriesPage />} />
         <Route path="/about" element={<AboutPage />} />
---
+++ src/container/product-details/ProductDetails.tsx
@@ -0,0 +1,14 @@
+import { type FC } from "react";
+import { type ProductInfoType } from "../../types/types";
+
+export const ProductDetails: FC<ProductInfoType> = ({
+  id,
+  title,
+  price,
+  category,
+  description,
+  image,
+  rating,
+}) => {
+  return <></>;
+};
